e844503d19051894b9c1f64fad56aed8f9afe493
James Sutton <james@algorithmia.com>
2020-03-27 16:00:46 -0600

    ensuring we only get published versions

--- algorithmia_ci/publish_algo.py
+++ algorithmia_ci/publish_algo.py
@@ -5,7 +5,7 @@ def publish_algo(mgmt_api_key, api_address, algo_schema, algo_name, algo_hash):
     client = Algorithmia.client(api_key=mgmt_api_key, api_address=api_address)
 
     algo = client.algo("{}/{}".format(algo_name, algo_hash))
-    results = algo.versions(1).results
+    results = algo.versions(1, published=True).results
     if len(results) > 0:
         cur_version = results[0]['version_info']
         print("--- last release version : {} ---".format(cur_version))
@@ -16,5 +16,5 @@ def publish_algo(mgmt_api_key, api_address, algo_schema, algo_name, algo_hash):
         raise Exception("{} is not considered a valid algorithm version schema".format(algo_schema))
     print("--- releasing new {}".format(algo_schema))
     algo.publish(version_info={"version_type": algo_schema, "release_notes": "automatically deployed by CI"})
-    latest_version = algo.versions(1).results[0]['version_info']
+    latest_version = algo.versions(1, published=True).results[0]['version_info']
     print("--- new version {} successfully published".format(latest_version))
