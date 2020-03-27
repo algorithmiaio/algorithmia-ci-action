4ee2326f8356cc4f3cb92e0ed188594e3bad99b8
James Sutton <james@algorithmia.com>
2020-03-27 14:23:15 -0600

    finished correcting errors in pipeline

--- algorithmia_ci/test_algo.py
+++ algorithmia_ci/test_algo.py
@@ -7,14 +7,13 @@ def parse_json(stringdata):
         data = json.loads(stringdata)
         return data
     except Exception:
-        raise Exception("{} is not json compatible, please check")
+        raise Exception("{} is not json compatible, please check", stringdata)
 
 
 def test_algo(regular_api_key, api_address, case_data, algo_name, algo_hash):
     client = Algorithmia.client(api_key=regular_api_key, api_address=api_address)
-    cases = parse_json(case_data)
     failures = []
-    for case in cases:
+    for case in case_data:
         input = case['input']
         expected = case['expected_output']
         name = case['case_name']
